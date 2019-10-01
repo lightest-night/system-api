30580c004e98c251495cd64ddc06b3da2ebeb3e6
James Woodley <james@lightestnight.com>
2019-10-01 14:16:34 +0100

    Actually assign serializer settings to serializer

--- Api/Serializer.cs
+++ Api/Serializer.cs
@@ -9,34 +9,37 @@ namespace LightestNight.System.Api
 {
     public class Serializer : IRestSerializer
     {
-        public string ContentType { get; set; } = "application/json";
+        private const string JsonContentType = "application/json";
+        private readonly JsonSerializerSettings _serializerSettings;
+        
+        public string ContentType { get; set; } = JsonContentType;
         public string[] SupportedContentTypes { get; }
         public DataFormat DataFormat { get; }
 
         public Serializer(Action<JsonSerializerSettings> settingsAction = null, DataFormat dataFormat = DataFormat.Json, params string[] supportedContentTypes)
         {
-            var settings = new JsonSerializerSettings
+            _serializerSettings = new JsonSerializerSettings
             {
                 Formatting = Formatting.Indented,
                 TypeNameHandling = TypeNameHandling.None,
                 ContractResolver = new CamelCasePropertyNamesContractResolver()
             };
             
-            settingsAction?.Invoke(settings);
+            settingsAction?.Invoke(_serializerSettings);
 
             DataFormat = dataFormat;
             SupportedContentTypes = supportedContentTypes;
-            if (!SupportedContentTypes.Contains("application/json"))
-                SupportedContentTypes = SupportedContentTypes.Union(new[] {"application/json"}).ToArray();
+            if (!SupportedContentTypes.Contains(JsonContentType))
+                SupportedContentTypes = SupportedContentTypes.Union(new[] {JsonContentType}).ToArray();
         }
 
         public string Serialize(object obj)
-            => JsonConvert.SerializeObject(obj);
+            => JsonConvert.SerializeObject(obj, _serializerSettings);
 
         public T Deserialize<T>(IRestResponse response)
-            => JsonConvert.DeserializeObject<T>(response.Content);
+            => JsonConvert.DeserializeObject<T>(response.Content, _serializerSettings);
 
         public string Serialize(Parameter parameter)
-            => JsonConvert.SerializeObject(parameter.Value);
+            => JsonConvert.SerializeObject(parameter.Value, _serializerSettings);
     }
 }
